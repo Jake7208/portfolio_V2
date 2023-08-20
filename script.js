ff649561ffac58e895a5c809413d462b3a8cfd72
Jake7208 <jake7208@gmail.com>
2023-08-19 21:53:25 -0600

    added main hero section

--- script.js
+++ script.js
@@ -2,34 +2,40 @@ let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
 let logoSpan = document.querySelectorAll('.logo');
 
-window.addEventListener('DOMContentLoaded', () => 
+const cursor = () =>
 {
-    setTimeout(() => {
-        
-        logoSpan.forEach((span, index) => 
-        {
-            setTimeout(() => 
-            {
-                span.classList.add('active');
-            }, (index + 1) * 400)
-        })
+    
+}
 
-        setTimeout(() =>
-        {
-            logoSpan.forEach((span, index) =>
-            {
-                setTimeout(() =>
-                {
+window.addEventListener('DOMContentLoaded', () => {
+    // Store the original overflow value
+    const originalOverflow = document.body.style.overflow;
+
+    // Set overflow to 'hidden'
+    document.body.style.overflow = 'hidden';
+
+    setTimeout(() => {
+        logoSpan.forEach((span, index) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (index + 1) * 400);
+        });
+
+        setTimeout(() => {
+            logoSpan.forEach((span, index) => {
+                setTimeout(() => {
                     span.classList.remove('active');
                     span.classList.add('fade');
-                }, (index + 1) * 50)
-            })
-        }, 2000)
+                }, (index + 1) * 100);
+            });
+        }, 2000);
 
-        setTimeout(() => 
-        {
-            intro.style.top = '-100vh';
+        setTimeout(() => {
+            intro.style.top = '-120%';
             intro.style.borderRadius = '80%';
-        }, 2300)
-    })
-})
+            
+            // Reset overflow to its original value
+            document.body.style.overflow = originalOverflow;
+        }, 2300);
+    });
+});
